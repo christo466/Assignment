65a876791050ad2bea0c25311acf5ca283bf9ed1
christo466 <113782762+christo466@users.noreply.github.com>
2022-09-25 21:09:21 +0530

    Create class1.py

---
+++ class1.py
@@ -0,0 +1,12 @@
+#step index 
+b="pythongrapcis"
+print(b[1])
+print(b[4])
+print(b[1:5:1])
+print(b[1:5:2])
+print(b[1:5:3])
+print(b[1:9:2])
+print(b[1:9:2])
+print(b[1:9:3])
+print(b[:-2])
+print(b[:-3])
